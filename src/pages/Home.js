09a7899870e23ba92266e4ae1039cbb40eb3f28c
meriem-ui <73528967+meriem-ui@users.noreply.github.com>
2022-10-04 16:12:59 +0200

    Update Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,6 @@
+<input  type="submit" name = "send" value = "Capsules"></input>
+<input  type="submit" name = "send" value = "Launches"></input>
+
+
+<Link to='/Capsules' class="Clink">Capsules</Link>
+<Link to='/Launches' class="Clink">Launches</Link>
