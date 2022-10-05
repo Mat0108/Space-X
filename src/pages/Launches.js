f9ff8fa3befc054a4c1ddb5491c20f44651d73ef
Mat0108 <61587759+Mat0108@users.noreply.github.com>
2022-10-05 11:32:01 +0200

    Update Launches.js

--- src/pages/Launches.js
+++ src/pages/Launches.js
@@ -26,7 +26,7 @@ const Launches = () => {
                 <div className="card col-6  launches-card">
                     <div className="card-body launches-body ">
                         <h5 className="card-title ">
-                        {/* {launche.details} */}
+                        { {launche.details}}
                             </h5>
                             
                         <div class="card-text col-8 launches-text">
