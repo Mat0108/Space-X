b5b1a845cc2e1404ad964f212fd059604e9ea1aa
Mat0108 <61587759+Mat0108@users.noreply.github.com>
2022-10-05 11:55:43 +0200

    update

--- src/pages/Launches.js
+++ src/pages/Launches.js
@@ -21,7 +21,7 @@ const Launches = () => {
         <div className="container">
             
             <h1 className="text"><Link class="href" to='/'>{fleche} </Link>Liste des Launches </h1>
-        <div className="row align-items-start  ">
+        <div className="row launches-row ">
             {Launches.map((launche) =>
                 <div className="card col-6  launches-card">
                     <div className="card-body launches-body ">
@@ -31,22 +31,17 @@ const Launches = () => {
                             </h5>
                             
                         <div class="card-text col-8 launches-text">
-                            <div>
-                                <p>Premier Stage : {launche.rocket.first_stage.cores[0].core_serial } </p>
-                                <p>Second Stage : {launche.rocket.second_stage.payloads[0].payload_id} </p>
-                                <p>Flight number : {launche.flight_number} - Launch year : {launche.launch_year} </p>
-                                <p>Rocket name : {launche.rocket.rocket_name} - Rocket type : {launche.rocket.rocket_type} </p>
-                                {launche.rocket.second_stage.payloads[0].payload_type !== null && <p>Payload type : {launche.rocket.second_stage.payloads[0].payload_type} - Orbit : {launche.rocket.second_stage.payloads[0].orbit} </p>}
-                                {launche.rocket.first_stage.cores[0].landing_type !== null && <p >Landing type : {launche.rocket.first_stage.cores[0].landing_type} </p>}
-                                {launche.rocket.first_stage.cores[0].landing_vehicle !== null && <p>Landing vehicle : {launche.rocket.first_stage.cores[0].landing_vehicle} </p>}
-                                <p>Site name : {launche.launch_site.site_name_long} </p>
-                                
-                
-                            </div>
+                            <p>Premier Stage : {launche.rocket.first_stage.cores[0].core_serial } </p>
+                            <p>Second Stage : {launche.rocket.second_stage.payloads[0].payload_id} </p>
+                            <p>Flight number : {launche.flight_number} - Launch year : {launche.launch_year} </p>
+                            <p>Rocket name : {launche.rocket.rocket_name} - Rocket type : {launche.rocket.rocket_type} </p>
+                            {launche.rocket.second_stage.payloads[0].payload_type !== null && <p>Payload type : {launche.rocket.second_stage.payloads[0].payload_type} - Orbit : {launche.rocket.second_stage.payloads[0].orbit} </p>}
+                            {launche.rocket.first_stage.cores[0].landing_type !== null && <p >Landing type : {launche.rocket.first_stage.cores[0].landing_type} </p>}
+                            {launche.rocket.first_stage.cores[0].landing_vehicle !== null && <p>Landing vehicle : {launche.rocket.first_stage.cores[0].landing_vehicle} </p>}
+                            <p>Site name : {launche.launch_site.site_name_long} </p>
                         </div>
                         <div class="">
-                            
-                                <img src={launche.links.mission_patch} alt={launche.mission_name} class=" col-7 launches-image" ></img>
+                            <img src={launche.links.mission_patch} alt={launche.mission_name} class=" col-7 launches-image" ></img>
                         </div>
                     </div>
                 </div>
