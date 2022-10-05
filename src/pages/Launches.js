da870cf0cc07f2f95e8bf7fe9f2ea6508219d960
Mat0108 <61587759+Mat0108@users.noreply.github.com>
2022-10-05 11:43:30 +0200

    update launches

--- src/pages/Launches.js
+++ src/pages/Launches.js
@@ -3,6 +3,7 @@ import './css/launches.css';
 
 import { useEffect, useState } from 'react';
 import { getLaunches } from '../services/space-x';
+import { Link } from "react-router-dom";
 
 const Launches = () => {
         const [Launches, setLaunches] = useState([]);
@@ -15,10 +16,11 @@ const Launches = () => {
         }, []);
     console.log(Launches);
 
+    let fleche = "<--";
     return (
         <div className="container">
             
-        <h1 className="">Liste des Launches </h1>
+            <h1 className="text"><Link class="href" to='/'>{fleche} </Link>Liste des Launches </h1>
         <div className="row align-items-start  ">
             {Launches.map((launche) =>
                 <div className="card col-6  launches-card">
@@ -32,16 +34,12 @@ const Launches = () => {
                             <div>
                                 <p>Premier Stage : {launche.rocket.first_stage.cores[0].core_serial } </p>
                                 <p>Second Stage : {launche.rocket.second_stage.payloads[0].payload_id} </p>
-                                <p>Flight number : {launche.flight_number} </p>
-                                <p>Launch year : {launche.launch_year} </p>
-                                <p>Rocket name : {launche.rocket.rocket_name} </p>
-                                <p>Rocket type : {launche.rocket.rocket_type} </p>
-                                {launche.rocket.second_stage.payloads[0].payload_type !== null && <p>Payload type : {launche.rocket.second_stage.payloads[0].payload_type} </p>}
-                                {launche.rocket.second_stage.payloads[0].orbit !== null && <p>Orbit : {launche.rocket.second_stage.payloads[0].orbit} </p>}
+                                <p>Flight number : {launche.flight_number} - Launch year : {launche.launch_year} </p>
+                                <p>Rocket name : {launche.rocket.rocket_name} - Rocket type : {launche.rocket.rocket_type} </p>
+                                {launche.rocket.second_stage.payloads[0].payload_type !== null && <p>Payload type : {launche.rocket.second_stage.payloads[0].payload_type} - Orbit : {launche.rocket.second_stage.payloads[0].orbit} </p>}
                                 {launche.rocket.first_stage.cores[0].landing_type !== null && <p >Landing type : {launche.rocket.first_stage.cores[0].landing_type} </p>}
                                 {launche.rocket.first_stage.cores[0].landing_vehicle !== null && <p>Landing vehicle : {launche.rocket.first_stage.cores[0].landing_vehicle} </p>}
                                 <p>Site name : {launche.launch_site.site_name_long} </p>
-                                {launche.details !== null && <p class="lh-1">Détails : {launche.details}</p>}
                                 
                 
                             </div>
