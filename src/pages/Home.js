f5eece238d97b0a53b203303def733cb0bc07956
Mat0108 <mesjeux0108@gmail.com>
2022-10-06 15:07:54 +0200

    modification page home et readme

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,28 @@ import './css/home.css';
 
 const Home = () => {
     return(
- 
-        <><Link class ="styled" to='/Capsules'>Capsules</Link>
         
-        <Link class ="styled" to='/Launches'>Launches</Link></>
+        <>
+        <h5 className="text">Space X wiki</h5>
+        <div className='div row'>
+            <div className='col-3'></div>
+            <div className='col-2 size'>                
+                <div className='row'>
+                    <Link className ="styled launche col-6" to='/Capsules'>Capsules</Link>
+                    <img src={"https://library.kissclipart.com/20180830/hbe/kissclipart-space-capsule-vector-clipart-space-capsule-spacecr-a896752b7621df22.png"} alt={"fusée"} class="col-3 image capsule-image" ></img>
+                </div>
+            </div>
+            
+            <div className='col-2'></div>
+            <div className='col-2 size'>
+                <div className='row'>
+                    <Link className ="styled launche col-6" to='/Launches'>Launches</Link>
+                    <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQ8ZxnYNep4PQ5mW2tRlXmCoH-ecARSBjdsc4h3aeEGCWE1QdXgv5PvKxoF9BBBXgKyf8&usqp=CAU"} alt={"fusée"} class="col-3 image" ></img>
+            
+                </div>
+            </div>
+        </div>
+        </>
         
 
     )
